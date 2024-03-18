ca3db9bfcd50226144f8e23a4251e30dadc0c236
Aditya Thebe <contact@adityathebe.com>
2024-03-18 10:35:03 +0545

    chore: refactor

--- Makefile
+++ Makefile
@@ -3,5 +3,6 @@ boostrap:
 	./bootstrap.sh
 
 .PHONY: secrets
+secrets:
 	rsync -azvP ~/.ssh/config ~/.ssh/adityathebe desktop:~/.ssh/
 	rsync -azvP ~/.config/secrets ~/.config/
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,27 +5,32 @@ primary_monitor=DP-5
 
 sudo pacman -Syu
 
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDs5rFHygPS8uCK+LJ4XOpenVrGk6ZUzNLb6w9eFiUI8 adityathebe' >> ~/.ssh/authorized_keys
+
 # Essentials
 sudo pacman -S base-devel linux linux-headers linux-firmware zsh git neovim \
-  pulseaudio curl
+  pulseaudio curl numlockx 
 
 # Install yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/yay.gita --depth=1 /tmp/yay
+cd /tmp/yay
 makepkg -si
+cd ~
 
 # GUI
 yay -S xorg-server xorg-init xorg-xrandr xclip bspwm pasystray \
   rofi sxhkd polybar dunst flameshot picom xset feh \
-  redshift-gtk blueman-applet nm-applet 
+  redshift-gtk blueman-applet nm-applet mpv nemo alacritty \
+  brave-bin visual-studio-code-bin slack-desktop \
+  gnome-keyring seahorse lxappearance
 
-# User applications
-yay -S tmux unzip go numlockx newsboat bottom yazi mpv wget \
-  eza bat lsof atuin restic nemo direnv zoxide fastfetch \
-  bandwhich alacritty brave-bin visual-studio-code-bin
-
-# Kubernetes
-yay -S flux helm kubectl
+# CLI applications
+yay -S tmux unzip go rust rust-analyzer newsboat bottom yazi wget \
+  eza bat lsof atuin restic direnv zoxide fastfetch bandwhich less rsync \
+  lazygit
+  
+# Containers
+yay -S flux helm kubectl docker docker-compose lazydocker
 
 # Set zsh as the default shell
 chsh -s $(which zsh)
@@ -37,8 +42,9 @@ fi
 ###################
 ## Setup Configs
 ###################
-git clone 'https://github.com/adityathebe/dotfiles' ~/dotfiles
+git clone 'https://github.com/adityathebe/dotfiles' ~/dotfiles --depth=1
 cp -r ~/dotfiles/.config/bspwm ~/.config
+cp -r ~/dotfiles/.config/fastfetch ~/.config
 cp -r ~/dotfiles/.config/git ~/.config
 cp -r ~/dotfiles/.config/newsboat ~/.config
 cp -r ~/dotfiles/.config/rofi ~/.config
@@ -49,15 +55,18 @@ cp ~/dotfiles/.config/hacking_aliases.sh ~/.config/.aliases
 cp ~/dotfiles/.config/kube_aliases.sh ~/.config/.aliases
 cp ~/dotfiles/.xinitrc ~/.xinitrc
 
+# Alacritty
+cp -r ~/dotfiles/.config/alacritty ~/.config
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes --depth=1
+
 # Polybar
 cp -r ~/dotfiles/.config/polybar ~/.config
 mkdir -p ~/.local/bin
 cp -r ~/dotfiles/.local/bin/statusbar ~/.local/bin/
 
 # zsh
-cp -r ~/dotfiles/.config/zsh ~/.config
+cp -r ~/dotfiles/.config/zsh/* ~/.config
 git clone https://github.com/romkatv/powerlevel10k.git ~/.local/share/zsh/powerlevel10k --depth=1 
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.local/share/zsh/oh-my-zsh --depth=1
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.local/share/zsh/plugins/zsh-autosuggestions --depth=1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.local/share/zsh/plugins/zsh-syntax-highlighting --depth=1
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.local/share/zsh/plugins/you-should-use --depth=1
