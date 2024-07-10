6d6b0957233bc6494176a95d92343e76b45c8135
Aditya Thebe <contact@adityathebe.com>
2024-07-11 00:06:15 +0545

    chore: add few applications

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,8 +27,8 @@ yay -S xorg-server xorg-init xorg-xrandr xclip bspwm pasystray \
 # CLI applications
 yay -S tmux unzip go rust rust-analyzer newsboat bottom yazi wget \
   eza bat lsof atuin restic direnv zoxide fastfetch bandwhich less rsync \
-  lazygit nix golangci-lint-bin jq go-yq aria2c
-  
+  lazygit nix golangci-lint-bin jq go-yq aria2 bind betterlockscreen
+
 # Containers
 yay -S fluxcd helm kubectl kubens kustomize docker docker-compose lazydocker
 
